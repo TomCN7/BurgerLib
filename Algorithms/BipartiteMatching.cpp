9d21981998f772d8141726d94c660631a57f1a14
Ming Zhang <mingzhang@tom.local>
2013-10-12 18:01:54 +0800

    bipartite matching modify

--- Algorithms/BipartiteMatching.cpp
+++ Algorithms/BipartiteMatching.cpp
@@ -27,7 +27,7 @@ bool DfsFind(int i)
 int Matching()
 {
 	memset(nMatching, -1, sizeof(nMatching));
-	int nTotal =0 ;
+	int nTotal = 0;
 	for (int i = 0; i < nX; ++i)
 	{
 		memset(bSearched, 0, sizeof(bSearched));
@@ -43,12 +43,12 @@ int Matching()
 int G[nX][nY];  //graph(left node nx, right node ny)
 int nX, nY;
 int cx[nX], cy[nX];  //cx[i]与xi顶点匹配的Y顶点
-int pred[N], mk[N], open[N];
+int pred[N], mark[N], open[N];
 int BFS_Matching()
 {		//最优匹配
 	int nAns = 0, nCur = 0, nTail = 0, t = 0;
 
-	memset(mk, 0xff, sizeof(mk));
+	memset(mark, 0xff, sizeof(mark));
 	memset(cx, 0xff, sizeof(cx));
 	memset(cy, 0xff, sizeof(cy));
 
@@ -59,9 +59,9 @@ int BFS_Matching()
 		{
 			for (int u = open[nCur], v = 0; v < nY && cx[i] == -1; ++v)
 			{
-				if (G[u][v] && mk[v] != i)
+				if (G[u][v] && mark[v] != i)
 				{
-					mk[v] = i; 
+					mark[v] = i; 
 					open[++nTail] = cy[v];
 					if (open[nTail] >= 0)
 					{
@@ -69,7 +69,7 @@ int BFS_Matching()
 						continue;
 					}
 
-					for (int d = u, e = v; d != -1;
+					for (int d = u, e = v; d != -1; 
 						t = cx[d], cx[d] = e, cy[e] = d, e = t, d = pred[d]);
 				}
 			}
